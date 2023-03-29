00e9deb2b76acd4c67950b05e99ba027287b90a9
John Goh Rengwu <johngohrw@gmail.com>
2023-03-30 06:25:30 +0800

    feat: handlebars, templates, data

---
+++ data.js
@@ -0,0 +1,85 @@
+export const data = {
+    "work": {
+        "flanksource": {
+            "title": "Flanksource",
+            "subtitle": "Front-End Engineer & Designer",
+            "location": "(Remote) Johannesburg, South Africa",
+            "date": "August 2021 - August 2022",
+            "items": [
+                "Design and FE development for Canary Checker, a Kubernetes multi-tenant synthetic monitoring system. Also contributed tooling, architectural and component design for various projects in React.",
+                "Designed high-fidelity prototypes and conducted heuristic evaluations on new features."
+            ]
+        },
+        "tdcx": {
+            "title": "TDCX",
+            "subtitle": "Front-End Developer",
+            "location": "Kuala Lumpur, Malaysia",
+            "date": "June 2020 - July 2021",
+            "items": [
+                "Developed Flash Coach, a virtual coaching and training platform for employees.",
+                "Web-related maintenance and enhancements; including FE implementation for a major redesign of the company website, automating CMS migrations and backups with Python, and integrating analytical tools."
+            ]
+        },
+        "nixel": {
+            "title": "Nixel Tech",
+            "subtitle": "Software Engineer and Designer",
+            "location": "Kuala Lumpur, Malaysia",
+            "date": "February 2019 - May 2020",
+            "items": [
+                "Design and FE development for a variety of web projects.",
+                "Maintained a high-traffic news portal and content creation platform, TanTanNews and MOPress.",
+                "Led, managed, and mentored a small team of intern developers."
+            ]
+        },
+        "accendo": {
+            "title": "Accendo Technologies",
+            "subtitle": "Intern Software Developer",
+            "location": "Kuala Lumpur, Malaysia",
+            "date": "December 2017 to March 2018",
+            "items": [
+                "FE development with React in an Agile team. Responsible for a print-friendly report generation feature.",
+                "Developed a web prototype in Vue.js for a talent management system for IJM Malaysia."
+            ]
+        }
+    },
+    "education": {
+        "monash": {
+            "title": "Monash University",
+            "subtitle": "Bachelor of Computer Science",
+            "location": "Kuala Lumpur, Malaysia",
+            "date": "February 2016 to May 2019",
+            "items": [
+                "Majored in Advanced Algorithms.",
+                "Winner of MDEC's ‘The Grand Challenge 2018’ Hackathon from MDEC Malaysia.",
+                "Highest scorer in the unit Advanced Programming (FIT3140) during Semester 1, 2018 for all Monash campuses worldwide."
+            ]
+        }
+    },
+    "skills": {
+        "javascript": 5,
+        "react": 5,
+        "python": 5,
+        "html/scss": 5,
+        "next.js": 4,
+        "tailwind": 4,
+        "figma": 4,
+        "linux": 4,
+        "selenium": 3,
+        "jest": 3,
+        "docker": 3,
+        "eslint": 3,
+        "typescript": 3,
+        "vue/vuex": 2,
+        "redux": 2,
+        "svelte": 2,
+        "opencv": 2,
+        "nginx": 2,
+        "flask": 2,
+        "websockets": 1,
+        "firebase": 1,
+        "three.js": 1,
+        "sql": 1,
+        "graphql": 1,
+        "rxjs": 1,
+    }
+}
