c7d7f944957a8e87d25bac0d9ce3860c047ad128
John Goh <johngohrw@gmail.com>
2023-05-15 18:13:37 +0800

    feat: localization, CN locale

---
+++ assets/data/cn.js
@@ -0,0 +1,84 @@
+export const data = {
+    "work": {
+        "flanksource": {
+            "title": "Flanksource",
+            "subtitle": "前端工程师 / 设计师",
+            "location": "（远程）南非, 约翰内斯堡",
+            "date": "2021年8月 - 2022年8月",
+            "items": [
+                "为Canary Checker设计和开发前端, 这是一个Kubernetes多租户合成监控系统。还为React的各种项目做出了工具, 架构和组件设计的贡献。",
+                "设计了高保真原型, 并对新功能进行了启发式评估。"
+            ]
+        },
+        "tdcx": {
+            "title": "TDCX",
+            "subtitle": "前端开发人员",
+            "location": "马来西亚, 吉隆坡",
+            "date": "2020年6月 - 2021年7月",
+            "items": [
+                "开发了Flash Coach,这是一个为员工提供虚拟教练和培训的平台。",
+                "进行与网站相关的维护和增强工作,包括为公司网站的重大重新设计实施前端,使用Python实现CMS迁移和备份自动化,并集成分析工具。"
+            ]
+        },
+        "nixel": {
+            "title": "Nixel Tech",
+            "subtitle": "软件工程师 / 设计师",
+            "location": "马来西亚, 吉隆坡",
+            "date": "2019年2月 - 2020年5月",
+            "items": [
+                "为多个Web项目进行设计和前端开发。",
+                "维护高流量的新闻门户网站和内容创建平台TanTanNews和MOPress。",
+                "领导、管理和指导一小组实习开发人员。"
+            ]
+        },
+        "accendo": {
+            "title": "Accendo Technologies",
+            "subtitle": "实习软件开发人员",
+            "location": "马来西亚, 吉隆坡",
+            "date": "2017年12月 - 2018年3月",
+            "items": [
+                "在一个敏捷团队中使用React进行前端开发。负责打印友好的报告生成功能。",
+                "为IJM马来西亚的人才管理系统开发了一个Vue.js的Web原型。"
+            ]
+        }
+    },
+    "education": {
+        "monash": {
+            "title": "蒙纳士大学 (Monash University)",
+            "subtitle": "计算机科学学士 (Bachelor of Computer Science)",
+            "location": "马来西亚",
+            "date": "February 2016 - May 2019",
+            "items": [
+                "获得马来西亚MDEC“2018年大挑战”黑客马拉松比赛冠军。",
+                "在2018年第一学期全球范围内,成为蒙纳士所有校区Advanced Programming (FIT3140)课程中得分最高的学生。"
+            ]
+        }
+    },
+    "skills": {
+        "javascript": 5,
+        "react": 5,
+        "python": 5,
+        "next.js": 4,
+        "tailwind": 4,
+        "figma": 4,
+        "linux": 4,
+        "svelte": 3,
+        "typescript": 3,
+        "vue/vuex": 3,
+        "eslint": 3,
+        "selenium": 3,
+        "jest": 3,
+        "redux": 2,
+        "opencv": 2,
+        "docker": 2,
+        "nginx": 2,
+        "flask": 2,
+        "rust": 2,
+        "three.js": 1,
+        "websockets": 1,
+        "graphql": 1,
+        "firebase": 1,
+        "java": 1,
+        "sql": 1,
+    }
+}
